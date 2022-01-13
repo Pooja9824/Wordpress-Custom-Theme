fc08f350d47d4f60e06f30bb3ebf53f1fa3209fa
Jap <jeet63240@gmail.com>
2022-01-13 17:55:42 +0530

    Put Dynamic path for stylesheet and images.

--- index.php
+++ index.php
@@ -1,33 +1,32 @@
-    <?php
-      get_header();
-    ?>
-    <div class="i1" style="width: 99.65%;height: 400px;border:solid 2px;background-image: url(wp-content/themes/NatureTheam/des5.png); background-repeat: no-repeat;
+   <!-- Header -->
+   <?php get_header(); ?>
+
+   <!-- Main -->
+   <div class="i1" style="width: 99.65%;height: 400px;border:solid 2px;background-image: url(<?php echo get_template_directory_uri(); ?>/images/des5.png); background-repeat: no-repeat;
 	background-attachment: fixed;background-size: 100% 100%;"></div>
 
-	<div class="continer">
-                  
+   <div class="continer">
 
-    <div class="cal1">
-   
-                 <h2>Box-1</h2>
-    
-    </div>
 
-    <div class="cal2">
-                 
-                  <h2>Box-2</h2>
-   
-    </div>
+     <div class="cal1">
 
-    <div class="cal3">
-                   
-                  <h2>Box-3</h2>
-    </div>
+       <h2>Box-1</h2>
 
-</div>
-</div>
+     </div>
 
-<?php
-  get_footer();
-?>
-   
+     <div class="cal2">
+
+       <h2>Box-2</h2>
+
+     </div>
+
+     <div class="cal3">
+
+       <h2>Box-3</h2>
+     </div>
+
+   </div>
+   </div>
+
+   <!-- Footer -->
+   <?php get_footer(); ?>
